cfb1c3b2c66005b47a09d421e07975c439e0aabd
Megha-Joshi <megha24joshi@gmail.com>
2022-05-12 01:40:47 +0530

    feat: providers and routers added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from "./logo.png";
 import { Navbar } from "./note-components/Navbar/navbar";
 import {Landingpage} from "./note-components/Landingpage/landingpage";
 import { Homepage } from "./note-components/Homepage/homepage";
+import { Login } from "./note-components/Authentication/login";
+import { Signup } from "./note-components/Authentication/signup";
+import { Route, Routes} from "react-router-dom";
+import { Trash } from "./note-components/Trash/trash";
+import { Archive } from "./note-components/Archive/archive";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/home" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/trash" element={<Trash />} />
+        <Route path="/archive" element={<Archive />}/>
+      </Routes>
     </div>
   );
 }
