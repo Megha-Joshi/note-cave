fa5abc60d206b660339fb9526adfdddc1ad2c0c4
Megha-Joshi <megha24joshi@gmail.com>
2022-05-14 19:50:55 +0530

    feat: label-added

--- src/note-components/Label/label.jsx
+++ src/note-components/Label/label.jsx
@@ -1,8 +1,23 @@
 import { Navbar } from "../Navbar/navbar";
 import { Sidebar } from "../Sidebar/sidebar";
 import "../Label/label.css"
+import { useNote } from "../../Context/note-context";
 
 const Label = () => {
+    const { tagItem, setTagItem } = useNote();
+    const addTagFunction = (e) => {
+        if(e.target.value !== "" && e.key === "Enter"){
+            setTagItem([...tagItem, e.target.value]);
+            e.target.value = "";
+        }
+        else{
+            setTagItem([...tagItem]);
+        }
+    }
+
+    const deleteTagFunction = (indexDeleted) => {
+        setTagItem(tagItem.filter((_,index) => index !== indexDeleted));
+    }
 return (
 <div className="App">
     <Navbar />
@@ -13,11 +28,18 @@ return (
                 <div class="chips-container">
                     <div class="container-one">
                         <div class="items">
-                            <div class="btn">
-                                <p class="first">Lemon</p>
-                                <img src="https://api.iconify.design/maki:cross-11.svg" alt="cross" class="image" />
+                            <div class="chips-btn">
+                                <ul>
+                                    {tagItem.map((item,index) => (
+                                    <li class="first">{item}
+                                    <button className="icon-no-bg" onClick={()=> deleteTagFunction(index)}><i class="fad fa-times-circle"></i></button>
+                                    </li>) 
+                                    )}
+                                    
+                                </ul>
+                                
                             </div>
-                            <p>New fruit...</p>
+                            <input placeholder="Enter new label" onKeyUp={addTagFunction}/>
                         </div>
                     </div>
                 </div>
